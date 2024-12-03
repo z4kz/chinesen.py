eeeea715497396a949217edee3d2a4571eed0374
zak storer <92131024+z4kz@users.noreply.github.com>
2024-12-03 04:17:37 -0700

    create chinesen.py

---
+++ chinesen.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""Print the chinese number system."""
+
+print('一 二 三 四 五 六 七 八 九 十') 
+print('1  2  3  4  5  6  7  8  9  10')
+print('')
+print('百')
+print('100')
+print('')
+print('千')
+print('1000')
+print('')
+print('万')
+print('10⁴')
+print('')
+print('亿')
+print('10⁸')
